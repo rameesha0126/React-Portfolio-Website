9cc291fcaff430d1650171ff5fcd982b1a595d27
Padmatilaka R W <110711978+IT21802430@users.noreply.github.com>
2024-09-07 11:03:56 +0530

    Skills section

--- src/components/LineGradient.jsx
+++ src/components/LineGradient.jsx
@@ -1,3 +1,5 @@
-const LineGradient = () => {} 
+const LineGradient = ({ width = "w-full" }) => {
+    return <div className={`h-0.5 ${width} bg-gradient-rainblue`} />
+} 
 
 export default LineGradient;
