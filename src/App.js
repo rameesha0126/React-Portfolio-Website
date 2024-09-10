9d5fc85093066c90607e73d4a24da22d7541acf1
Padmatilaka R W <110711978+IT21802430@users.noreply.github.com>
2024-09-10 14:16:35 +0530

    Testimonials section

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Landing from "./scenes/Landing";
 import LineGradient from "./components/LineGradient";
 import MySkills from "./scenes/MySkills";
 import Projects from "./scenes/Projects";
+import Testimonials from "./scenes/Testimonials";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState("home");
@@ -45,6 +46,14 @@ function App() {
       <div className="w-5/6 mx-auto">
         <Projects />
       </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto">
+        <Testimonials />
+      </div>
+      {/* <LineGradient />
+      <div className="w-5/6 mx-auto">
+        <Testimonials />
+      </div> */}
     </div>
   );
 }
