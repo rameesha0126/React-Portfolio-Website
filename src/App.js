97858305f20aa50b8370b3c59bd48a2ba5459c9c
Padmatilaka R W <110711978+IT21802430@users.noreply.github.com>
2024-09-11 08:20:44 +0530

    Contact and Footer section

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import LineGradient from "./components/LineGradient";
 import MySkills from "./scenes/MySkills";
 import Projects from "./scenes/Projects";
 import Testimonials from "./scenes/Testimonials";
+import Contact from "./scenes/Contact";
+import Footer from "./scenes/Footer";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState("home");
@@ -50,10 +52,11 @@ function App() {
       <div className="w-5/6 mx-auto">
         <Testimonials />
       </div>
-      {/* <LineGradient />
+      <LineGradient />
       <div className="w-5/6 mx-auto">
-        <Testimonials />
-      </div> */}
+        <Contact />
+      </div>
+      <Footer />
     </div>
   );
 }
